4d59c7ff84822a2cbe239b915e7ba6e68f1bf0be
gandesandeep <gandesandeep3@gmail.com>
2016-03-19 02:13:19 -0400

    Update ProgrammingAssignmentLexicalScoping.R

--- ProgrammingAssignmentLexicalScoping.R
+++ ProgrammingAssignmentLexicalScoping.R
@@ -5,22 +5,23 @@
 makeCacheMatrix <- function(x = matrix()) {
   
   inv <- NULL
-  inputset <- function(y) {
+  inputset <- function(y) {                           ## set the value of the matrix
     x <<- y
     inv <<- NULL
   }
-  inputget <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  inputget <- function() x                            ## get the value of the matrix
+  setinverse <- function(inverse) inv <<- inverse     ## set the value of the matrix inversion
+  getinverse <- function() inv                        ## get the value of the matrix inversion
   list(set=inputset, get=inputget, setinverse=setinverse, getinverse=getinverse)
   
 }
 
 # 2) cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
-
+## Look for cached inversion first and use it if found, otherwise it
+## calculates the inversion. 
 cacheSolve <- function(x, ...) {
   
-  inv <- x$getinverse()
+  inv <- x$getinverse()                               ## Check if cached value exists
   if(!is.null(inv)) {
     message("getting cached data...")
     return(inv)
